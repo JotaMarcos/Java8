6377c8f0aa62f54cd7471f8747cd63d71a441f90
João Marcos Aires Duarte <joao.duarte@ITBAM.ORG.BR>
2021-12-22 15:57:39 -0400

    Review Streams

--- src/ExemploCurso.java
+++ src/ExemploCurso.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.IntStream;
 
 class Curso {
 	private String nome;
@@ -37,6 +38,18 @@ public class ExemploCurso {
 		// cursos.forEach(c -> System.out.println(c.getNome())); // aqui temos que passar o lambda
 		
 		// Utilizando a interface fluente stream
+		//cursos.stream().filter(c -> c.getAlunos() > 100).forEach(c -> System.out.println(c.getNome()));
+		
+//		cursos.stream()
+//		   .filter(c -> c.getAlunos() > 100)
+//		   .map(c -> c.getAlunos())
+//		   .forEach(x -> System.out.println(x));
+		
+//		cursos.stream()
+//		.filter(c -> c.getAlunos() > 100)
+//		.map(Curso::getAlunos)
+//		.forEach(System.out::println);
+//		
 //		cursos.stream().filter(c -> c.getAlunos() >= 100)
 //		.forEach(c -> System.out.println(c.getNome())); 
 		
@@ -45,11 +58,22 @@ public class ExemploCurso {
 //		.map(Curso::getAlunos)
 //		.forEach(total -> System.out.println(total));
 		
-		cursos.sort(Comparator.comparing(Curso::getAlunos));
+//		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+//		
+//		int sum = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).sum();
+//		System.out.println(sum);
 		
-		int sum = cursos.stream().filter(c -> c.getAlunos() >= 100).mapToInt(Curso::getAlunos).sum();
-		System.out.println(sum);
 		
+		//Streams primitivos
+		IntStream stream = cursos.stream()
+				   .filter(c -> c.getAlunos() > 100)
+				   .mapToInt(Curso::getAlunos);
+		
+		int soma = cursos.stream()
+				   .filter(c -> c.getAlunos() > 100)
+				   .mapToInt(Curso::getAlunos)
+				   .sum();
+		System.out.println("Total de Cursos com mais de 100 alunos: " + soma + " alunos.");
 		
 		
 		
