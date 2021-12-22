46acb7b5d6f33afd8887a081d3bbeb8911d76b54
João Marcos Aires Duarte <joao.duarte@ITBAM.ORG.BR>
2021-12-22 11:30:45 -0400

    Method Reference

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
 
@@ -27,18 +28,30 @@ public class OrdenaStrings {
 
 		// Novo forma de fazer a Lista de String
 		// Collections.sort(palavras, comparador);
-//		palavras.sort((s1, s2) -> {
-//			if (s1.length() < s2.length())
-//				return -1;
-//			if (s1.length() > s2.length())
-//				return 1;
-//			return 0;
-//		});
-		
-		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
-		
+		palavras.sort((s1, s2) -> {
+			if (s1.length() < s2.length())
+				return -1;
+			if (s1.length() > s2.length())
+				return 1;
+			return 0;
+		});
+
+		// palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
+		// Utilizando Lambda
+		// palavras.sort(Comparator.comparing(s -> s.length()));
+
+		palavras.sort(Comparator.comparing(String::length));
+
+		Function<String, Integer> funcao = String::length;
+		Function<String, Integer> funcao2 = s -> s.length();
+
+		Comparator<String> comparador1 = Comparator.comparing(funcao);
+		Comparator<String> comparador2 = Comparator.comparing(funcao2);
+		palavras.sort(comparador1);
+		palavras.sort(comparador2);
+
 		System.out.println(palavras);
-		
 
 		// System.out.println("===========================================================");
 		// Forma antiga de varrer a Lista
@@ -56,21 +69,19 @@ public class OrdenaStrings {
 //
 //		Consumer<String> impressor = s -> System.out.println(s);
 //		palavras.forEach(impressor);
-		
+
+		Consumer<String> impressor = System.out::println;
+		palavras.forEach(System.out::println);
+
 		System.out.println("===========================================================");
 		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 		palavras.sort((s1, s2) -> s1.length() - s2.length());
-		
-		
+
 		palavras.forEach(palavra -> System.out.println(palavra));
-		
-		
-		
 
 		System.out.println("===========================================================");
 		new Thread(() -> System.out.println("Executando um Runnable")).start();
 
-		
 	}
 }
 
