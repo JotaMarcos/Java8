7390a095156116197f56b23569adbcb99e7aa2c9
João Marcos Aires Duarte <joao.duarte@ITBAM.ORG.BR>
2021-12-22 09:50:33 -0400

    Aplicação do Lambda

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -7,73 +7,82 @@ import java.util.function.Consumer;
 public class OrdenaStrings {
 
 	public static void main(String[] args) {
-		
+
 		// Forma antiga que era feita a Lista de String
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("alura online");
 		palavras.add("editora casa do código");
 		palavras.add("caelum");
-		
+
 		Collections.sort(palavras);
 		System.out.println(palavras);
-		
+
 		System.out.println("===========================================================");
-		
-		
-		Comparator<String> comparador1 = new ComparadorPorTamanho();
-		Collections.sort(palavras, comparador1);
-		System.out.println(palavras);
-		
-		System.out.println("===========================================================");
-		
+
+//		Comparator<String> comparador1 = new ComparadorPorTamanho();
+//		Collections.sort(palavras, comparador1);
+//		System.out.println(palavras);
+
+//		System.out.println("===========================================================");
+
 		// Novo forma de fazer a Lista de String
-		
-		Comparator<String> comparador2 = new ComparadorPorTamanho();
 		// Collections.sort(palavras, comparador);
-		palavras.sort(comparador2);
+//		palavras.sort((s1, s2) -> {
+//			if (s1.length() < s2.length())
+//				return -1;
+//			if (s1.length() > s2.length())
+//				return 1;
+//			return 0;
+//		});
+		
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
 		System.out.println(palavras);
 		
-		System.out.println("===========================================================");
+
+		// System.out.println("===========================================================");
 		// Forma antiga de varrer a Lista
 //		for (String p : palavras) {
 //			System.out.println(p);
 //		}
+
+//		System.out.println("===========================================================");
+//		// Nova foram de varrer a Lista
+//		Consumer<String> consumidor1 = new ImprimiNaLinha();
+//		palavras.forEach(consumidor1);
+
+//		System.out.println("===========================================================");
+//		// Primeiro para o Lambda
+//
+//		Consumer<String> impressor = s -> System.out.println(s);
+//		palavras.forEach(impressor);
 		
 		System.out.println("===========================================================");
-		// Nova foram de varrer a Lista
-		Consumer<String> consumidor = new ImprimiNaLinha();
-		palavras.forEach(consumidor);
-		
-		
-		
-		
-		
-		
-	}
+		palavras.forEach((String s) -> System.out.println(s));
 
+	}
 }
 
+// Quando é adicionada a classe anonima essa classe pode ser deleta, pois não preciso mais dela
+//class ImprimiNaLinha implements Consumer<String>{
+//
+//	@Override
+//	public void accept(String s) {
+//		System.out.println(s);
+//		
+//	}
+//	
+//}
 
-class ImprimiNaLinha implements Consumer<String>{
-
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-		
-	}
-	
-}
-
-
-class ComparadorPorTamanho implements Comparator<String>{
-	
-	@Override
-	public int compare(String s1, String s2) {
-		if (s1.length() < s2.length()) 
-			return -1;
-		if(s1.length() > s2.length())
-			return 1;
-		return 0;
-	}
-	
-}
+//class ComparadorPorTamanho implements Comparator<String> {
+//
+//	@Override
+//	public int compare(String s1, String s2) {
+//		if (s1.length() < s2.length())
+//			return -1;
+//		if (s1.length() > s2.length())
+//			return 1;
+//		return 0;
+//	}
+//
+//}
